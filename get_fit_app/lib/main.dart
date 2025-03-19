d9037f407a0d269dc75b2df26d413ee7dc7d332d
Athish <amita004@d.umn.edu>
2025-03-19 15:27:33 -0500

    Fixed the error on import  firebase core

--- get_fit_app/lib/main.dart
+++ get_fit_app/lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'view/view.dart';
 
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+
 void main() {
   runApp(const MyApp());
 }
