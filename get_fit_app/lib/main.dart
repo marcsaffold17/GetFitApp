1f8b4ef7081eaf64c6970117852756cab34dcc21
Athish <amita004@d.umn.edu>
2025-03-19 16:04:36 -0500

    Revert "removed weird stash thing"
    This reverts commit 71874a45e0a956a5ca8b8b3e2d1fd1bb84dd81f3.

--- get_fit_app/lib/main.dart
+++ get_fit_app/lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+<<<<<<< Updated upstream
 import 'view/view.dart';
+=======
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package: firebase_core/firebase_core.dart';
+>>>>>>> Stashed changes
 
 void main() {
   runApp(const MyApp());
