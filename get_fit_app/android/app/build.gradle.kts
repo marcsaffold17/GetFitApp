0b1e1cf2b93f1402b91cc6c68176ead85f8bd7f8
Aswath-sudhakar <amita003@d.umn.edu>
2025-03-26 13:11:47 -0600

    made a basic function graph line graph display

--- get_fit_app/android/app/build.gradle.kts
+++ get_fit_app/android/app/build.gradle.kts
@@ -12,7 +12,7 @@ dependencies {
 android {
     namespace = "com.example.get_fit_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
--- get_fit_app/lib/presenter/presenter.dart
+++ get_fit_app/lib/presenter/presenter.dart
@@ -1,1 +1,1 @@
-import '../model/model.dart';
+
