40aad953f261f31c102b5b7039f31ded9b83b5d8
msaffold <saffo026@d.umn.edu>
2025-03-27 16:45:04 -0500

    Decoupled main.dart and insert_workout_view.dart

--- get_fit_app/lib/main.dart
+++ get_fit_app/lib/main.dart
@@ -1,58 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-import '../model/insert_workout_model.dart';
+// Don't need in main
 import '../view/insert_workout_view.dart';
+import '../model/insert_workout_model.dart';
 import '../presenter/insert_workout_presenter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+    // Branch exclusive code to showcase screen
     final repository = WorkoutRepository();
     final presenter = WorkoutPresenter(repository);
-
+    //
     return MaterialApp(
-      title: 'Workout App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomeScreen(presenter: presenter),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  final WorkoutPresenter presenter;
-
-  const HomeScreen({Key? key, required this.presenter}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Workout App'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => WorkoutEntryScreen(
-                    presenter: presenter,
-                  )),
-            );
-          },
-          child: Text('Add Workout'),
-        ),
-      ),
+      title: 'Flutter MVP Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: WorkoutEntryScreen(presenter: presenter),
     );
   }
 }
