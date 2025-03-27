64da4a1b3d3ab9ff9fcb9528c3e7193956ddcaf0
Aswath-sudhakar <amita003@d.umn.edu>
2025-03-27 14:40:35 -0600

    changed files to work in the MVP architecture

--- get_fit_app/lib/main.dart
+++ get_fit_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:fl_chart/fl_chart.dart';
+import 'view/chart_veiw.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,142 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Fl Graph',
+      title: 'MVP Chart Example',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Line Chart Demo'),
-    );
-  }
-}
-
-class LineChartSample extends StatelessWidget {
-  const LineChartSample({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 300,
-      child: LineChart(
-        LineChartData(
-          gridData: const FlGridData(show: true),
-          titlesData: const FlTitlesData(show: true),
-          borderData: FlBorderData(show: true),
-          lineBarsData: [
-            LineChartBarData(
-              spots: const [
-                FlSpot(0, 1),
-                FlSpot(1, 1.5),
-                FlSpot(2, 1.4),
-                FlSpot(3, 3.4),
-                FlSpot(4, 2),
-                FlSpot(5, 2.2),
-                FlSpot(6, 1.8),
-              ],
-              isCurved: true,
-              color: Colors.blue,
-              barWidth: 4,
-              belowBarData: BarAreaData(show: false),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class BarChartSample extends StatelessWidget {
-  const BarChartSample({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 300,
-      child: BarChart(
-        BarChartData(
-          barGroups: [
-            BarChartGroupData(
-              x: 0,
-              barRods: [BarChartRodData(toY: 1, color: Colors.blue)],
-            ),
-            BarChartGroupData(
-              x: 1,
-              barRods: [BarChartRodData(toY: 1.5, color: Colors.blue)],
-            ),
-            BarChartGroupData(
-              x: 2,
-              barRods: [BarChartRodData(toY: 1.4, color: Colors.blue)],
-            ),
-            BarChartGroupData(
-              x: 3,
-              barRods: [BarChartRodData(toY: 3.4, color: Colors.blue)],
-            ),
-            BarChartGroupData(
-              x: 4,
-              barRods: [BarChartRodData(toY: 2, color: Colors.blue)],
-            ),
-            BarChartGroupData(
-              x: 5,
-              barRods: [BarChartRodData(toY: 2.2, color: Colors.blue)],
-            ),
-            BarChartGroupData(
-              x: 6,
-              barRods: [BarChartRodData(toY: 1.8, color: Colors.blue)],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  String _selectedChart = 'Line';
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            DropdownButton<String>(
-              value: _selectedChart,
-              onChanged: (String? newValue) {
-                setState(() {
-                  _selectedChart = newValue!;
-                });
-              },
-              items:
-                  <String>['Line', 'Bar'].map<DropdownMenuItem<String>>((
-                    String value,
-                  ) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-            ),
-            const SizedBox(height: 20),
-            _selectedChart == 'Line'
-                ? const LineChartSample()
-                : const BarChartSample(),
-          ],
-        ),
-      ),
+      home: const ChartScreen(),
     );
   }
 }
---
+++ get_fit_app/lib/view/chart_veiw.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
+import '../presenter/chart_presenter.dart';
+import '../model/chart_model.dart';
+
+class ChartScreen extends StatefulWidget {
+  const ChartScreen({super.key});
+
+  @override
+  State<ChartScreen> createState() => _ChartScreenState();
+}
+
+class _ChartScreenState extends State<ChartScreen> implements ChartView {
+  String _selectedChart = 'Line';
+  List<ChartModel> _chartData = [];
+  late ChartPresenter _presenter;
+
+  @override
+  void initState() {
+    super.initState();
+    _presenter = ChartPresenter(this);
+    _presenter.loadData();
+  }
+
+  @override
+  void updateChart(List<ChartModel> data) {
+    setState(() {
+      _chartData = data;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Flutter MVP Chart')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            DropdownButton<String>(
+              value: _selectedChart,
+              onChanged: (String? newValue) {
+                setState(() {
+                  _selectedChart = newValue!;
+                });
+              },
+              items:
+                  ['Line', 'Bar'].map((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+            ),
+            const SizedBox(height: 20),
+            if (_chartData.isNotEmpty)
+              _selectedChart == 'Line'
+                  ? LineChartWidget(data: _chartData)
+                  : BarChartWidget(data: _chartData),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LineChartWidget extends StatelessWidget {
+  final List<ChartModel> data;
+
+  const LineChartWidget({super.key, required this.data});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 300,
+      child: LineChart(
+        LineChartData(
+          lineBarsData: [
+            LineChartBarData(
+              spots: data.map((e) => FlSpot(e.x.toDouble(), e.y)).toList(),
+              isCurved: true,
+              color: Colors.blue,
+              barWidth: 4,
+              belowBarData: BarAreaData(show: false),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BarChartWidget extends StatelessWidget {
+  final List<ChartModel> data;
+
+  const BarChartWidget({super.key, required this.data});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 300,
+      child: BarChart(
+        BarChartData(
+          barGroups:
+              data
+                  .map(
+                    (e) => BarChartGroupData(
+                      x: e.x,
+                      barRods: [BarChartRodData(toY: e.y, color: Colors.blue)],
+                    ),
+                  )
+                  .toList(),
+        ),
+      ),
+    );
+  }
+}
