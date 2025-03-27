2a14d370274f0a8d145bd60e779d89792f6a2b8c
Athish <amita004@d.umn.edu>
2025-03-27 14:15:52 -0500

    added exercise model.dart to be able to read the json files

---
+++ get_fit_app/lib/model/exercies_model.dart
@@ -0,0 +1,30 @@
+class Exercise {
+  final String name;
+  final String type;
+  final String muscle;
+  final String equipment;
+  final String difficulty;
+  final String instructions;
+
+  Exercise({
+    required this.name,
+    required this.type,
+    required this.muscle,
+    required this.equipment,
+    required this.difficulty,
+    required this.instructions,
+  });
+
+  factory Exercise.fromJson(Map<String,dynamic> json)
+  {
+    return Exercise(
+      name: json['name'],
+      type: json['type'],
+      muscle: json['muscle'],
+      equipment: json['equipment'],
+      difficulty: json['difficulty'],
+      instructions: json['instructions'],
+    );
+  }
+
+}
