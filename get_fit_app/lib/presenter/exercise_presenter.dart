4abfffb83717d5bf9409aeb04cfb732d8eb10ba9
Athish <amita004@d.umn.edu>
2025-03-27 22:00:54 -0500

    added code for exercise_presenter.dart

---
+++ get_fit_app/lib/presenter/exercise_presenter.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../model/exercies_model.dart';
+
+abstract class ExerciseView {
+  void showExercises(List<Exercise> exercises);
+  void showError(String message);
+}
+
+class ExercisePresenter {
+  final ExerciseView view;
+  final String apiKey = "1k1ZlfCKgi6LiAm0AtKgVw==wm9LHZEOibILwX0u"; 
+
+  ExercisePresenter(this.view);
+
+  Future<void> fetchMuscleExercises(String muscle) async {
+    final url = Uri.parse("https://api.api-ninjas.com/v1/exercises?muscle=$muscle");
+
+    try {
+      final response = await http.get(
+        url,
+        headers: {"X-Api-Key": apiKey},
+      );
+
+      if (response.statusCode == 200) {
+        List<dynamic> jsonData = json.decode(response.body);
+        List<Exercise> exercises = jsonData.map((e) => Exercise.fromJson(e)).toList();
+        view.showExercises(exercises);
+      } else {
+        view.showError("Failed to load exercises");
+      }
+    } catch (e) {
+      view.showError("An error occurred: $e");
+    }
+  }
+}
