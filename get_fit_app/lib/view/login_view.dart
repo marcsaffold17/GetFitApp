d3a0e72f017219b1575de223b88c248d0162f3df
Athish <amita004@d.umn.edu>
2025-04-07 22:55:03 -0500

    Added login account page changes

--- get_fit_app/lib/view/login_view.dart
+++ get_fit_app/lib/view/login_view.dart
@@ -10,11 +10,12 @@ class LoginButtonPage extends StatelessWidget {
       // appBar: AppBar(title: const Text('')),
       body: Container(
         decoration: const BoxDecoration(
+          // color: Color.fromARGB(255, 29, 28, 28)
           gradient: LinearGradient(
             colors: [Color.fromARGB(255, 4, 222, 138), Color.fromARGB(255, 2, 154, 80), Color.fromARGB(255, 1, 50, 34)],
-            //colors: [Color.fromARGB(255, 67, 66, 66), Color.fromARGB(255, 189, 172, 172), Color.fromARGB(255, 60, 60, 61)], 
-            begin: Alignment.centerLeft, // Start position
-            end: Alignment.centerRight, // End position
+            // colors: [Color.fromARGB(255, 67, 66, 66), Color.fromARGB(255, 189, 172, 172), Color.fromARGB(255, 60, 60, 61)], 
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight, 
           ),
         ),
         child: Column(
@@ -43,7 +44,7 @@ class LoginButtonPage extends StatelessWidget {
                 );
               },
               style: ElevatedButton.styleFrom(
-                minimumSize: const Size(250, 50), // Adjust width and height
+                minimumSize: const Size(250, 50), 
                 // padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 0),
               ),
               child: const Text('Create Account', style: TextStyle(fontWeight: FontWeight.w600, fontFamily: 'Mirage', fontSize: 25),),
@@ -60,7 +61,7 @@ class LoginButtonPage extends StatelessWidget {
                 );
               },
               style: ElevatedButton.styleFrom(
-                minimumSize: const Size(250, 50), // Adjust width and height
+                minimumSize: const Size(250, 50),
                 // padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 0),
               ),
               child: const Text('Login',style: TextStyle(fontWeight: FontWeight.w600, fontFamily: 'Mirage', fontSize: 25) ),
@@ -119,14 +120,65 @@ class LoginPage extends State<MyLoginPage> implements LoginView {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+      flexibleSpace: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 4, 222, 138),
+              Color.fromARGB(255, 2, 154, 80),
+              Color.fromARGB(255, 1, 50, 34),
+            ],
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+          ),
+        ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.end,
+    ),
+      body: Stack(
+      children: [
+        Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 4, 222, 138),
+                Color.fromARGB(255, 2, 154, 80),
+                Color.fromARGB(255, 1, 50, 34),
+              ],
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+            ),
+          ),
+        ),
+        Align(
+          alignment: Alignment.topLeft,
+          child: RichText(
+            text: TextSpan(
+              style: TextStyle(
+                fontFamily: 'Voguella',
+                fontSize: 40,
+                color: const Color.fromARGB(255, 255, 255, 255),
+              ),
+              children: const <TextSpan>[
+                TextSpan(text: '   Login\n'),
+              ],
+            ),
+          ),
+        ),
+        Positioned(
+          left: -5,
+          right: -5,
+          top: 200,
+          bottom: -15,
+          child: Container(
+            padding: const EdgeInsets.all(16.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(width: 5.0, color: Colors.white),
+              borderRadius: BorderRadius.all(Radius.circular(30.0)),
+            ),
+            child: SingleChildScrollView( 
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             TextField(
               controller: userNameText,
@@ -170,6 +222,10 @@ class LoginPage extends State<MyLoginPage> implements LoginView {
           ],
         ),
       ),
+          )
+        )
+      ]
+      )
     );
   }
 }
@@ -321,18 +377,32 @@ Widget build(BuildContext context) {
                       hintText: 'Confirm Password',
                     ),
                   ),
-                  SizedBox(height: 12),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      padding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0), 
-                      shape: RoundedRectangleBorder( 
-                        borderRadius: BorderRadius.circular(30.0),
-                      ),                    ),
-                    onPressed: handleCreateAccount, 
-                    child: const Text
-                    ( 
-                      'Create Account',
-                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold), 
+                  SizedBox(height: 30),
+                  Container(
+                    width: 400, 
+                    height: 60, 
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [Color.fromARGB(255, 4, 222, 138), Color.fromARGB(255, 2, 154, 80), Color.fromARGB(255, 1, 50, 34)],
+                        //colors: [Color.fromARGB(255, 67, 66, 66), Color.fromARGB(255, 189, 172, 172), Color.fromARGB(255, 60, 60, 61)], 
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight, 
+                      ),
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.transparent,
+                        padding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
+                      ),
+                      onPressed: handleCreateAccount,
+                      child: const Text(
+                        'Create Account',
+                        style: TextStyle(fontFamily: 'Mirage', fontSize: 20, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 255, 255, 255)),
+                      ),
                     ),
                   )
                 ],
