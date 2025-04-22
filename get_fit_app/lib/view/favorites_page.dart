0c35634c1c719641a9c15fd2c5c1331c34a74e66
Logan Woods <woods539@d.umn.edu>
2025-04-22 14:40:57 -0500

    streamlined UI

--- get_fit_app/lib/view/favorites_page.dart
+++ get_fit_app/lib/view/favorites_page.dart
@@ -10,44 +10,28 @@ class FavoritesPage extends StatefulWidget {
 }
 
 class _FavoritesPageState extends State<FavoritesPage> {
-  List<Exercise> _favoriteExercises = [];
   Set<String> _WorkoutPlan = {};
   String reps = '';
   String sets = '';
   TextEditingController setsController = TextEditingController();
   TextEditingController repsController = TextEditingController();
 
-  final favoritesRef = FirebaseFirestore.instance.collection('Login-Info').doc(globalUsername).collection('favorites');
-  final workoutPlanRef = FirebaseFirestore.instance.collection('Login-Info').doc(globalUsername).collection('Workout-Plan');
+  final favoritesRef = FirebaseFirestore.instance
+      .collection('Login-Info')
+      .doc(globalUsername)
+      .collection('favorites');
+  final workoutPlanRef = FirebaseFirestore.instance
+      .collection('Login-Info')
+      .doc(globalUsername)
+      .collection('Workout-Plan');
 
   @override
   void initState() {
     super.initState();
-    _loadFavorites();
     _LoadWorkoutPlan();
-
   }
 
-  void _loadFavorites() async {
-    final snapshot = await favoritesRef.get();
-
-    setState(() {
-      _favoriteExercises = snapshot.docs.map((doc) {
-        final data = doc.data();
-        return Exercise(
-          name: data['name'],
-          type: data['type'],
-          muscle: data['muscle'],
-          difficulty: data['difficulty'],
-          equipment: data['equipment'],
-          instructions: data['instructions'],
-          isFavorite: true,
-        );
-      }).toList();
-    });
-  }
-
-  void _LoadWorkoutPlan() async{
+  void _LoadWorkoutPlan() async {
     final snapshot = await workoutPlanRef.get();
     setState(() {
       _WorkoutPlan = snapshot.docs.map((doc) => doc.id).toSet();
@@ -58,117 +42,142 @@ class _FavoritesPageState extends State<FavoritesPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 244, 238, 227),
-      body: _favoriteExercises.isEmpty
-          ? Center(
-        child: Text(
-          "No favorites right now",
-          style: TextStyle(fontSize: 18, color: Colors.grey),
-        ),
-      )
-          : ListView.builder(
-        itemCount: _favoriteExercises.length,
-        itemBuilder: (context, index) {
-          final exercise = _favoriteExercises[index];
+      body: StreamBuilder<QuerySnapshot>(
+        stream: favoritesRef.snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Center(child: CircularProgressIndicator());
+          }
 
-          return Card(
-            color: Color.fromARGB(255, 229, 221, 212),
-            margin: EdgeInsets.all(8),
-            child: ListTile(
-              title: Text(exercise.name, style: TextStyle(color: Color.fromARGB(255, 20, 50, 31)),),
-              subtitle: Text(
-                "Difficulty: ${exercise.difficulty}\n"
-                    "Equipment: ${exercise.equipment}",
-                style: TextStyle(color: Color.fromARGB(255, 46, 105, 70)),
+          final docs = snapshot.data!.docs;
+          if (docs.isEmpty) {
+            return Center(
+              child: Text(
+                "No favorites right now",
+                style: TextStyle(fontSize: 18, color: Colors.grey),
               ),
-              onTap: () => _showDetails(exercise),
-              trailing: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  IconButton(
-                    icon: Icon(Icons.delete, color: Color.fromARGB(255, 81, 163, 108)),
-                    onPressed: () async {
-                      await favoritesRef.doc(exercise.name).delete();
-                      setState(() {
-                        _favoriteExercises.removeAt(index);
-                      });
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(content: Text('${exercise.name} removed from favorites')),
-                      );
-                    },
-                  ),
-                  IconButton(
-                      icon: Icon(Icons.add_outlined),
-                      color: Color.fromARGB(255 ,81, 163, 108),
-                      onPressed: () async {
-                        final DateTime? pickedDate = await showDatePicker(
-                          context: context,
-                          initialDate: DateTime.now(),
-                          firstDate: DateTime(2000),
-                          lastDate: DateTime(2100),
-                        );
+            );
+          }
 
-                        await showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return AlertDialog(
-                              title: const Text('Edit Workout'),
-                              content: Column(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  TextField(
-                                    controller: setsController,
-                                    keyboardType: TextInputType.number,
-                                    decoration: const InputDecoration(labelText: 'Sets'),
-                                  ),
-                                  TextField(
-                                    controller: repsController,
-                                    keyboardType: TextInputType.number,
-                                    decoration: const InputDecoration(labelText: 'Reps'),
-                                  ),
-                                ],
+          final exercises = docs.map((doc) {
+            final data = doc.data() as Map<String, dynamic>;
+            return Exercise(
+              name: data['name'],
+              type: data['type'],
+              muscle: data['muscle'],
+              difficulty: data['difficulty'],
+              equipment: data['equipment'],
+              instructions: data['instructions'],
+              isFavorite: true,
+            );
+          }).toList();
 
-                              ),
-                              actions: [
-                                TextButton(
-                                  onPressed: () {
-                                    sets = setsController.text;
-                                    reps = repsController.text;
-                                    Navigator.of(context).pop();
-                                  },
-                                  child: Text('Proceed'),
-                                ),
-                              ],
-                            );
-                          },
-                        );
-                        if (pickedDate != null) {
-                          String formattedDate = DateFormat('MM-dd-yyyy').format(pickedDate);
-
-                          await workoutPlanRef.doc(formattedDate).set({
-                            'date': formattedDate,
-                          });
-                          await workoutPlanRef.doc(formattedDate).collection("Workout").doc(exercise.name).set({
-                            'name': exercise.name,
-                            'difficulty': exercise.difficulty,
-                            'equipment': exercise.equipment,
-                            'instructions': exercise.instructions,
-                            'date': formattedDate,
-                            'reps': reps,
-                            'sets': sets
-                          });
-                        };
-
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(content: Text('${exercise.name} added to workout plan for ${pickedDate?.day}/${pickedDate?.month}/${pickedDate?.year}')),
-                        );
-                      }
-                  ),
-                ],
-              ),
-
-            ),
+          return ListView.builder(
+            itemCount: exercises.length,
+            itemBuilder: (context, index) {
+              return _buildExerciseCard(exercises[index]);
+            },
           );
         },
+      ),
+    );
+  }
+
+  Widget _buildExerciseCard(Exercise exercise) {
+    return Card(
+      color: Color.fromARGB(255, 229, 221, 212),
+      margin: EdgeInsets.all(8),
+      child: ListTile(
+        title: Text(exercise.name, style: TextStyle(color: Color.fromARGB(255, 20, 50, 31))),
+        subtitle: Text(
+          "Difficulty: ${exercise.difficulty}\nEquipment: ${exercise.equipment}",
+          style: TextStyle(color: Color.fromARGB(255, 46, 105, 70)),
+        ),
+        onTap: () => _showDetails(exercise),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            IconButton(
+              icon: Icon(Icons.delete, color: Color.fromARGB(255, 81, 163, 108)),
+              onPressed: () async {
+                await favoritesRef.doc(exercise.name).delete();
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('${exercise.name} removed from favorites')),
+                );
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.add_outlined),
+              color: Color.fromARGB(255, 81, 163, 108),
+              onPressed: () async {
+                final DateTime? pickedDate = await showDatePicker(
+                  context: context,
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime(2000),
+                  lastDate: DateTime(2100),
+                );
+
+                await showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: const Text('Edit Workout'),
+                      content: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          TextField(
+                            controller: setsController,
+                            keyboardType: TextInputType.number,
+                            decoration: const InputDecoration(labelText: 'Sets'),
+                          ),
+                          TextField(
+                            controller: repsController,
+                            keyboardType: TextInputType.number,
+                            decoration: const InputDecoration(labelText: 'Reps'),
+                          ),
+                        ],
+                      ),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            sets = setsController.text;
+                            reps = repsController.text;
+                            Navigator.of(context).pop();
+                          },
+                          child: Text('Proceed'),
+                        ),
+                      ],
+                    );
+                  },
+                );
+
+                setsController.clear();
+                repsController.clear();
+
+                if (pickedDate != null) {
+                  String formattedDate = DateFormat('MM-dd-yyyy').format(pickedDate);
+
+                  await workoutPlanRef.doc(formattedDate).set({
+                    'date': formattedDate,
+                  });
+                  await workoutPlanRef.doc(formattedDate).collection("Workout").doc(exercise.name).set({
+                    'name': exercise.name,
+                    'difficulty': exercise.difficulty,
+                    'equipment': exercise.equipment,
+                    'instructions': exercise.instructions,
+                    'date': formattedDate,
+                    'reps': reps,
+                    'sets': sets
+                  });
+
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('${exercise.name} added to workout plan for ${pickedDate.day}/${pickedDate.month}/${pickedDate.year}')),
+                  );
+                }
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -179,12 +188,12 @@ class _FavoritesPageState extends State<FavoritesPage> {
       builder: (context) {
         return AlertDialog(
           backgroundColor: Color.fromARGB(255, 244, 238, 227),
-          title: Text(exercise.name, style: TextStyle(color:  Color.fromARGB(255, 20, 50, 31)),),
+          title: Text(exercise.name, style: TextStyle(color: Color.fromARGB(255, 20, 50, 31))),
           content: Text(exercise.instructions, style: TextStyle(color: Color.fromARGB(255, 46, 105, 70))),
           actions: [
             TextButton(
               onPressed: () => Navigator.pop(context),
-              child: Text("Close",style: TextStyle(color:  Color.fromARGB(255, 46, 105, 70)),),
+              child: Text("Close", style: TextStyle(color: Color.fromARGB(255, 46, 105, 70))),
             ),
           ],
         );
